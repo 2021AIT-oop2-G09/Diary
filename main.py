c4ea7d0366fa299972542bd906c77c4a38e4dc90
Kamto01 <konanarasi@gmail.com>
2021-12-09 16:22:27 +0900

    自分の分追加したよ

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from diaries.DiarySample import DiarySample
-from diaries.NagataniDiary import NagataniDiary
+from diaries.KambaraDiary import KambaraDiary
 
 # ↓のリストには、メンバーの各日記が格納されます。
 diaries = [
   DiarySample(),
-  NagataniDiary(),
+  KambaraDiary(),
 ] 
 
 for d in diaries:
