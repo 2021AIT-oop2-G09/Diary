6f5c0762acbf56681ac6286cf677c69ce56c9254
kanada0904 <kanadakyoya@icloud.com>
2021-12-09 15:36:32 +0900

    new

---
+++ main.py
@@ -0,0 +1,15 @@
+from diaries.DiarySample import DiarySample
+from diaries.NagataniDiary import NagataniDiary
+
+# ↓のリストには、メンバーの各日記が格納されます。
+diaries = [
+  DiarySample(),
+  NagataniDiary(),
+] 
+
+for d in diaries:
+    print("---------------------------------")
+    print(d.get_date())
+    print(d.get_summary())
+    print(d.get_author())
+    print()
