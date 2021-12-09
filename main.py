adab6725438f7f8a5c3aa316c59b8a0954e72e15
Kawakami-Takuma <95839484+Kawakami-Takuma@users.noreply.github.com>
2021-12-09 16:07:06 +0900

    日記追加

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from diaries.DiarySample import DiarySample
-from diaries.NagataniDiary import NagataniDiary
+from diaries.TakumaDiary import TakumaDiary
 
 # ↓のリストには、メンバーの各日記が格納されます。
 diaries = [
   DiarySample(),
-  NagataniDiary(),
+  TakumaDiary(),
 ] 
 
 for d in diaries:
