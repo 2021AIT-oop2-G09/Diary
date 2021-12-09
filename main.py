7e7a980c1da0900a85930cefeeb35e5f3ce2b4b7
yua0220 <Kyua12345@icloud.com>
2021-12-09 16:00:37 +0900

    追加

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from diaries.DiarySample import DiarySample
-from diaries.NagataniDiary import NagataniDiary
+from diaries.KurataDiary import KurataDiary
 
 # ↓のリストには、メンバーの各日記が格納されます。
 diaries = [
   DiarySample(),
-  NagataniDiary(),
+  KurataDiary(),
 ] 
 
 for d in diaries:
