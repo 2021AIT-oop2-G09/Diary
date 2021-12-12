e9af7390f4201c6a71739aa7845a4788ce7d9d58
kanada0904 <kanadakyoya@icloud.com>
2021-12-12 14:05:32 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from diaries.DiarySample import DiarySample
-
+from diaries.KanadaDiary import KanadaDiary
 
 # ↓のリストには、メンバーの各日記が格納されます。
 diaries = [
   DiarySample(),
+  KanadaDiary(),
 ] 
 
 for d in diaries:
